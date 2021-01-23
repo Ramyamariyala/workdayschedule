51b4b7c2014e894af72892b06b8cc51c4cd74870
Ramya.Mariyala <ramya.mariyala@gmail.com>
2021-01-23 10:39:55 -0500

    updated have errors

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,40 @@
 
-var d = new Date(),
-    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
-    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
-    // ampm = d.getHours() >= 12 ? 'pm' : 'am',
-    months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'],
-    days = ['Sunday ','Monday','Tuesday','Wednesday','Thurusday','Friday','Saturday'];
-    currentDateString = days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+"EST";
+var d = new Date();
+ var hours = d.getHours()
+ 
+ 
+ var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'],
+     days = ['Sunday ','Monday','Tuesday','Wednesday','Thurusday','Friday','Saturday'];
+    currentDateString = days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate();
     $("#currentDay").text(currentDateString);
 
    $(".saveBtn").on("click",function(event){
-    console.log(this);
-    var value = $(this).sibling(".description").val();
+  
+    var text = $(this).siblings(".description").val();
+    
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text)
+    
    })
 
-   $(".time-block").each(function(){
-       
+   $("textarea").each(function(){
+       var texthour =parseInt($(this).attr("value"));
+       if(texthour === hours){
+       $(this).addClass("present");
+       }
+       if (texthour < hours) {
+           $(this).addClass("past");
+       }
+       if (texthour > hours){
+           $(this).addClass("future");
+
+       }
+    
    })
+
+   var keys = Object.keys(localStorage);
+keys.forEach(displayEvents);
+
+function displayEvents(item) {
+    $(`*[data-hour="${item}"]`).val(JSON.parse(localStorage.getItem(`${item}`)));
+};
